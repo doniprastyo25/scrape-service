f0bc7d8871d9f67862ef58cd81a8344abc837d3b
doniprastyo25 <donismsbanking@gmail.com>
2023-07-01 14:18:57 +0700

    fix import and add models mongo

--- apps/scrape_apps/models.py
+++ apps/scrape_apps/models.py
@@ -1,3 +1,10 @@
-from django.db import models
-
+from mongoengine import Document , fields
 # Create your models here.
+
+class DataScrape(Document):
+    job = fields.StringField()
+    link = fields.URLField()
+    additional_info = fields.DictField()
+
+    def __str__(self):
+        return self.job
